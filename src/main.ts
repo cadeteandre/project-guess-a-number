9fa77e070794ccd0d627f489d34639089b7a8aa2
Andre Cadete <andrecadete70@gmail.com>
2024-10-18 18:27:36 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import './style.css'
+
+const inputGuess = document.querySelector('#inputGuess') as HTMLInputElement;
+const button = document.querySelector('button') as HTMLButtonElement;
+const showResults = document.querySelector('#showResults') as HTMLElement;
+const roundsContainer = document.querySelector('.rounds') as HTMLElement;
+const roundOptions = document.querySelectorAll('.roundsAmount') as NodeListOf<HTMLInputElement>;
+const randomNum: number = Math.floor(Math.random() * 100) +1;
+
+const howManyRounds = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  let roundsAmount: number = 0;
+  // ! still need to put the custom function
+  // if(target.value === 'custom') {
+  //   const inputCustom = document.createElement('input') as HTMLInputElement;
+  //   inputCustom.setAttribute('type', 'text');
+  //   roundsContainer.appendChild(inputCustom);
+  // }
+  
+  roundsAmount = Number(target.value);
+
+  let counter: number = 1;
+  button.addEventListener('click', () => {
+    const userGuess: number = Number(inputGuess.value);
+    const result = document.createElement('p') as HTMLParagraphElement;
+
+    if(counter < roundsAmount) {
+      switch(true) {
+        case (userGuess > 100):
+          result.textContent =  `${counter} - You need to guess a number between 1 and 100`;
+          roundsContainer.innerHTML = `${counter} / ${roundsAmount}`;
+          showResults.appendChild(result);
+          break;
+        case (userGuess < randomNum):
+          result.textContent = `${counter} - You need to guess higher than ${userGuess}. Try again...`;
+          roundsContainer.innerHTML = `${counter} / ${roundsAmount}`;
+          showResults.appendChild(result);
+          break;
+        case (userGuess > randomNum):
+          result.textContent = `${counter} - You need to guess lower than ${userGuess}. Try again...`;
+          roundsContainer.innerHTML = `${counter} / ${roundsAmount}`;
+          showResults.appendChild(result);
+          break;
+        case (userGuess === randomNum):
+          showResults.innerHTML = `<span class="victory">Congratulations!!</span> The number is ${randomNum}`
+          break;
+      };
+    } else {
+      roundsContainer.innerHTML = `${counter} / ${roundsAmount}`;
+      showResults.innerHTML = `
+        <p>You are out of moves :/</p>
+        <a class="yellowLink" href="index.html">Try again</a>
+      `;
+    }
+    inputGuess.value = '';
+    counter++;
+  });
+};
+
+for(let i: number = 0; i < roundOptions.length; i++) {
+  roundOptions[i].addEventListener('click', howManyRounds);
+};
